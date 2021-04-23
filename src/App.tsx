4d2cce082fc3aeab9ad3b36d36ae4073ad0637b8
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-23 12:15:33 -0600

    Create initial form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Container, Row, Col, Form } from 'react-bootstrap';
 
 export class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello world!</h1>
-      </div>
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col xs={8}><h1>Environment Canada CSV File Parser</h1></Col>
+          <Col></Col>
+          </Row>
+        <br />
+        <Row>
+          <Col></Col>
+          <Col xs={8}><Form>
+            <Form.Group>
+             <Form.File id="csvinput" label="Select CSV File Here" />
+          </Form.Group>
+          </Form></Col>
+          <Col></Col>
+          </Row>
+      </Container>
     )
   }
 };
