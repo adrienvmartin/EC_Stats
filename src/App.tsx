f37eeb5195af53ecab15c2fa94b3c5b5e50c37cf
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-23 16:45:08 -0600

    Create basic form layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,69 @@ export class App extends React.Component {
   render() {
     return (
       <Container>
-        <Row>
+        <Row className="justify-content-md-center">
           <Col></Col>
-          <Col xs={8}><h1>Environment Canada CSV File Parser</h1></Col>
+          <Col md={8}><h1>Environment Canada CSV File Parser</h1></Col>
           <Col></Col>
           </Row>
         <br />
         <Row>
           <Col></Col>
-          <Col xs={8}><Form>
+          <Col><Form>
             <Form.Group>
              <Form.File id="csvinput" label="Select CSV File Here" />
-          </Form.Group>
-          </Form></Col>
+            </Form.Group>
+          </Form>
+          </Col>
           <Col></Col>
-          </Row>
+        </Row>
+        <br />
+
+        <Row className="justify-content-md-center">
+          <Col sm={2}>
+                <Form.Check label={"Select All Months"} />
+          </Col>
+          <Col sm={2}>
+               <Form.Check label={"Select None"} />
+          </Col>
+      
+        </Row>
+        
+        <br />
+
+        <Row className="justify-content-md-center">
+          <Col md={3}>
+            <Form>
+              <Form.Group>
+                <Form.Check label={"January"} />
+                <Form.Check label={"February"} />
+                <Form.Check label={"March"} />
+                <Form.Check label={"April"} />
+              </Form.Group>
+              </Form>
+            </Col>
+          <Col md={3}>
+            <Form>
+              <Form.Group>
+              <Form.Check label={"May"} />
+              <Form.Check label={"June"} />
+              <Form.Check label={"July"} />
+              <Form.Check label={"August"} />
+              </Form.Group>
+              </Form>
+            </Col>
+          <Col md={3}>
+            <Form>
+              <Form.Group>
+              <Form.Check label={"September"} />
+              <Form.Check label={"October"} />
+              <Form.Check label={"November"} />
+              <Form.Check label={"December"} />
+              </Form.Group>
+              </Form>
+            </Col>
+        </Row>
+       
       </Container>
     )
   }
