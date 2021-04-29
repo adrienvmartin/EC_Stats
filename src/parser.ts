0debec59e846e2ae12548fc3ca3eac00bdf1859a
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-28 21:22:25 -0600

    Extract yearly array data into monthly segments

--- src/parser.ts
+++ src/parser.ts
@@ -1,17 +1,31 @@
 export const monthParser = (data: []) => {
-  const Jan: number[] = [];
-  const Feb: number[] = [];
+  // Add check for leap years
 
-  for (let i = 0; i < 31; i++) {
-    Jan.push(data[i]);
-  }
-
-  for (let i = 31; i < 28; i++) {
-    Feb.push(data[i]);
-  }
+  const Jan = data.slice(0, 31);
+  const Feb = data.slice(31, 60);
+  const Mar = data.slice(60, 91);
+  const Apr = data.slice(91, 121);
+  const May = data.slice(121, 152);
+  const Jun = data.slice(152, 182);
+  const Jul = data.slice(182, 213);
+  const Aug = data.slice(213, 244);
+  const Sep = data.slice(244, 274);
+  const Oct = data.slice(274, 305);
+  const Nov = data.slice(305, 335);
+  const Dec = data.slice(335, 366);
 
   return {
     Jan,
     Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec,
   };
 };
