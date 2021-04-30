97301767fa5e167c651e2c30d38b3f613228501e
Adrien Martin <adrien.v.martin@gmail.com>
2021-04-30 11:03:03 -0600

    Add key parameter to warmestHigh method and remove other methods temporarily

--- src/analyzers/StatsAnalyzer.ts
+++ src/analyzers/StatsAnalyzer.ts
@@ -1,21 +1,12 @@
 import { Month } from '../parser';
 
-export const warmestHigh = (month: Month): any => {
+// Add key to parameters so it can be used on any value (MaxTemp, MeanTemp, MinTemp, etc)
+// Only one "warmest" parser instead of one for every data type
+// Instead of key: string, make Enum of different keys in CsvObject
+export const warmestHigh = (month: Month, key: string): number => {
   const highs: number[] = [];
   month.forEach((m) => {
     highs.push(Number(m['MaxTemp(°C)']));
   });
   return Math.max(...highs);
-};
-
-export const coldestHigh = (highs: string[]) => {
-  return Math.min(Number(highs));
-};
-
-export const warmestLow = (lows: string[]) => {
-  return Math.max(Number(lows));
-};
-
-export const coldestLow = (lows: string[]) => {
-  return Math.min(Number(lows));
 };
