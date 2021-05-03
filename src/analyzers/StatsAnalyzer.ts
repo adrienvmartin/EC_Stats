99cfe5f47fa2440abbbea8f310aafc6bb4fa503e
Adrien Martin <adrien.v.martin@gmail.com>
2021-05-03 10:24:43 -0600

    Create new method for calculating coldest, and remove unused code

--- src/analyzers/StatsAnalyzer.ts
+++ src/analyzers/StatsAnalyzer.ts
@@ -1,23 +1,19 @@
 import { Month, CsvObject } from '../parser';
 
-// Add key to parameters so it can be used on any value (MaxTemp, MeanTemp, MinTemp, etc)
-// Only one "warmest" parser instead of one for every data type
-// Instead of key: string, make Enum of different keys in CsvObject
-export const warmestHigh = (month: Month, key: [keyof CsvObject]): number => {
-  const highs: number[] = [];
-  month.forEach((m) => {
-    highs.push(Number(m['MaxTemp(°C)']));
-  });
-  return Math.max(...highs);
-};
-
-// T = CsvObject, K extends the keys of CsvObject & uses them as its value for inputting which stat we want
 export class Calculator {
   warmest = <K extends keyof CsvObject>(month: Month, key: K): number => {
-    const highs: number[] = [];
+    const set: number[] = [];
     month.forEach((m) => {
-      highs.push(Number(m[key]));
+      set.push(Number(m[key]));
     });
-    return Math.max(...highs);
+    return Math.max(...set);
+  };
+
+  coldest = <K extends keyof CsvObject>(month: Month, key: K): number => {
+    const set: number[] = [];
+    month.forEach((m) => {
+      set.push(Number(m[key]));
+    });
+    return Math.min(...set);
   };
 }
