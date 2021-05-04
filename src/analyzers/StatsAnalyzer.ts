a67883117da5403799facfd8687fdd82bfcc0656
Adrien Martin <adrien.v.martin@gmail.com>
2021-05-03 19:04:43 -0600

    Write comments for possible ways to handle data

--- src/analyzers/StatsAnalyzer.ts
+++ src/analyzers/StatsAnalyzer.ts
@@ -2,6 +2,11 @@ import { Month, CsvObject } from '../parser';
 
 // Make Calculator generic class and then pass in key when creating new instance?
 // e.g. const coldCalc = new Calculator('MinTempC'); etc.?
+// Define key-value pair somewhere as such - checkbox key : CsvObject key
+//                                          (e.g. maxtemp: 'MaxTemp(C)')
+// Which is then used when the user selects which data they'd like
+//
+// Create method to get the date of warmest high, rather than just using warmest high of each month
 export class Calculator {
   // Returns warmest number for a particular metric (max temp, low temp, etc.) within a given month
   warmest = <K extends keyof CsvObject>(month: Month, key: K): number => {
