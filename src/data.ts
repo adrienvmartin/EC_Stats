c89825dd81fbc7f0dd1ef9c37be7671d9153dcb5
Adrien Martin <adrien.v.martin@gmail.com>
2021-05-04 20:03:06 -0600

    Create data file with relevant interfaces

---
+++ src/data.ts
@@ -0,0 +1,50 @@
+export interface Month {
+  'Longitude(x)': string;
+  'Latitude(y)': string;
+  StationName: string;
+  ClimateID: string;
+  'Date/Time': string;
+  Year: string;
+  Month: string;
+  Day: string;
+  DataQuality: string;
+  'MaxTemp(°C)': string;
+  MaxTempFlag: string;
+  'MinTemp(°C)': string;
+  MinTempFlag: string;
+  'MeanTemp(°C)': string;
+  MeanTempFlag: string;
+  'HeatDegDays(°C)': string;
+  HeatDegDaysFlag: string;
+  'CoolDegDays(°C)': string;
+  CoolDegDaysFlag: string;
+  'TotalRain(mm)': string;
+  TotalRainFlag: string;
+  'TotalSnow(cm)': string;
+  TotalSnowFlag: string;
+  'TotalPrecip(mm)': string;
+  TotalPrecipFlag: string;
+  'SnowonGrnd(cm)': string;
+  SnowonGrndFlag: string;
+  'DirofMaxGust(10s deg)': string;
+  DirofMaxGustFlag: string;
+  'SpdofMaxGust(km/h)': string;
+  SpdofMaxGustFlag: string;
+}
+
+export interface Year {
+  Jan: Month;
+  Feb: Month;
+  Mar: Month;
+  Apr: Month;
+  May: Month;
+  Jun: Month;
+  Jul: Month;
+  Aug: Month;
+  Sep: Month;
+  Oct: Month;
+  Nov: Month;
+  Dec: Month;
+}
+
+export type FullYear = Month[];
