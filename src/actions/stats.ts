c91bb40de1cd8df3ffcff55bb1fb0e761e0dd5e0
Adrien Martin <adrien.v.martin@gmail.com>
2021-05-05 16:48:37 -0600

    Refactor action creators to return proper data

--- src/actions/stats.ts
+++ src/actions/stats.ts
@@ -1,4 +1,5 @@
 import { parse, ParseResult } from 'papaparse';
+import React from 'react';
 import { Dispatch } from 'redux';
 import { CsvObject } from '../datatypes';
 import { ActionTypes } from './types';
@@ -8,13 +9,13 @@ export interface FetchStatsAction {
   payload: CsvObject[];
 }
 
-export const fetchStats = (e: React.DragEvent<HTMLDivElement>) => {
-  e.preventDefault();
+const createStatsArray = (e: React.DragEvent<HTMLDivElement>): CsvObject[] => {
+  let set: CsvObject[] = [];
   Array.from(e.dataTransfer.files)
     .filter((file) => file.type === 'text/csv')
     .forEach(async (file) => {
       const text = await file.text();
-      const result: ParseResult<CsvObject> = parse(text, {
+      const result: ParseResult<CsvObject> = await parse(text, {
         header: true,
         skipEmptyLines: false,
         transformHeader: (h) => {
@@ -25,11 +26,18 @@ export const fetchStats = (e: React.DragEvent<HTMLDivElement>) => {
           console.log('Parsing complete: \n', results);
         },
       });
-      return (dispatch: Dispatch) => {
-        dispatch<FetchStatsAction>({
-          type: ActionTypes.fetchStats,
-          payload: result.data,
-        });
-      };
+      set = result.data;
     });
+  return set;
+};
+
+// Put Array.from into separate function, call that function within fetchStats
+export const fetchStats = (e: React.DragEvent<HTMLDivElement>) => {
+  return async (dispatch: Dispatch) => {
+    const response = await createStatsArray(e);
+    dispatch<FetchStatsAction>({
+      type: ActionTypes.fetchStats,
+      payload: response,
+    });
+  };
 };
