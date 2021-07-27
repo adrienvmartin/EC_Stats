d07f3b81af5c777ee2d440a71baf9d4f18992b7e
Adrien Martin <adrien.v.martin@gmail.com>
2021-07-26 22:32:05 -0600

    Delete existing files in directory before uploading a new one

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 const cors = require('cors');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,9 +11,11 @@ const app = express();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
+const filepath = 'client/public/uploads';
+
 const storage = multer.diskStorage({
   destination: (req, res, cb) => {
-    cb(null, 'client/public');
+    cb(null, filepath);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalName);
@@ -21,12 +24,17 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage }).single('file');
 
-app.post('/takestats', async (req, res) => {
-  const data = await req.data;
-  res.send({ data: data });
-});
-
 app.post('/upload', (req, res) => {
+  fs.readdir(filepath, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join(filepath, file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+
   upload(req, res, (err) => {
     if (err instanceof multer.MulterError) {
       return res.status(500).json(err);
@@ -35,6 +43,13 @@ app.post('/upload', (req, res) => {
     }
     return res.status(200).send(req.file);
   });
+});
+
+app.post('/takestats', async (req, res) => {
+  const data = [];
+
+  const fname = req.fname;
+  fs.createReadStream(__dirname, +fname);
 });
 
 app.listen(PORT, () => {
