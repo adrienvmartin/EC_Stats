370c6eabee20b4a1977b4decf9334cfb817de0fd
Adrien Martin <adrien.v.martin@gmail.com>
2021-07-26 21:00:37 -0600

    Change port of backend server

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json({ extended: false }));
-app.use(express.static('client/build'));
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send({ express: 'YOUR BACKEND IS CONNECTED' });
 });
 
